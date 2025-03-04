8a861c908be8b72fb437c182bdb7f083aa1fbe52
Omar Abdulaziz <omarabdul3ziz@gmail.com>
2025-03-04 13:23:03 +0200

    use the new nft rules template

--- pkg/netlight/nft/nft.go
+++ pkg/netlight/nft/nft.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os/exec"
+	"text/template"
 
 	"github.com/rs/zerolog/log"
 	"github.com/vishvananda/netlink"
@@ -36,39 +37,13 @@ func Apply(r io.Reader, ns string) error {
 	return nil
 }
 
-func flushChain(ns, table, chain string) error {
-	args := []string{"flush", "chain", table, chain}
-	var cmd *exec.Cmd
-
-	if ns != "" {
-		cmd = exec.Command("ip", "netns", "exec", ns, "nft")
-		cmd.Args = append(cmd.Args, args...)
-	} else {
-		cmd = exec.Command("nft", args...)
-	}
-
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		return errors.Wrapf(err, "failed to flush nft chain: %v", string(out))
-	}
-
-	return nil
-}
-
 // DropTrafficToLAN drops all the outgoing traffic to any peers on
 // the same lan network, but allow dicovery port for ygg/myc by accepting
 // traffic to/from dest/src ports.
-// @th,0,16 and @th,16,16 is raw expression for sport/dport in transport header
-// used due to limitation on the installed nft v0.9.1
-func DropTrafficToLAN() error {
+func DropTrafficToLAN(rules string) error {
 	dgw, err := getDefaultGW()
 	if err != nil {
 		return fmt.Errorf("failed to find default gateway: %w", err)
-	}
-
-	// apply anyway even the if the gateway is private; to clean the previous rules
-	if err := flushChain("", "inet filter", "forward"); err != nil {
-		return err
 	}
 
 	if !dgw.IP.IsPrivate() {
@@ -78,30 +53,24 @@ func DropTrafficToLAN() error {
 
 	ipAddr := dgw.IP.String()
 	netAddr := getNetworkRange(dgw)
-	macAddr := dgw.HardwareAddr.String()
 	log.Debug().
 		Str("ipAddr", ipAddr).
 		Str("netAddr", netAddr).
-		Str("macAddr", macAddr).
 		Msg("drop traffic to lan with the default gateway")
 
-	var buf bytes.Buffer
-	buf.WriteString("table inet filter {\n")
-	buf.WriteString("  chain forward {\n")
-	// allow traffic on sport ygg/myc discovery ports
-	buf.WriteString("    meta l4proto { tcp, udp } @th,0,16 { 9651, 9650 } accept;\n")
-	// allow traffic on dport ygg/myc discovery ports
-	buf.WriteString("    meta l4proto { tcp, udp } @th,16,16 { 9651, 9650 } accept;\n")
-	// allow traffic to the default gateway
-	buf.WriteString(fmt.Sprintf("    ip daddr %s accept;\n", ipAddr))
-	// allow traffic to any ip not in the network range
-	buf.WriteString(fmt.Sprintf("    ip daddr != %s accept;\n", netAddr))
-	// only drop traffic if it destined to mac addr other than the default gateway
-	buf.WriteString(fmt.Sprintf("    ether daddr != %s drop;\n", macAddr))
-	buf.WriteString("  }\n")
-	buf.WriteString("}\n")
+	templ, err := template.New("lanSecurityRules").Parse(rules)
+	if err != nil {
+		return err
+	}
 
-	// applied on host
+	var buf bytes.Buffer
+	if err := templ.Execute(&buf, map[string]string{
+		"GatewayIP": ipAddr,
+		"SubnetIP":  netAddr,
+	}); err != nil {
+		return err
+	}
+
 	return Apply(&buf, "")
 }
 
